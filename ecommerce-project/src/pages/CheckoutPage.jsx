a44a964647686bbf8efddccd4e5a7fdf6e965cb6
NJ018 <nitinchillar015@gmail.com>
2026-09-19 14:35:36 +0530

    Generate html for homepage

--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,11 +1,10 @@
-import './checkout-header.css'
-import './CheckoutPage.css'
+import './checkout-header.css';
+import './CheckoutPage.css';
 
-export function CheckoutPage(){
+export function CheckoutPage() {
   return (
     <>
       <title>Checkout</title>
-
 
       <div className="checkout-header">
         <div className="header-content">
@@ -184,38 +183,38 @@ export function CheckoutPage(){
           </div>
 
           <div className="payment-summary">
-              <div className="payment-summary-title">
-                Payment Summary
-              </div>
+            <div className="payment-summary-title">
+              Payment Summary
+            </div>
 
-              <div className="payment-summary-row">
-                <div>Items (3):</div>
-                <div className="payment-summary-money">$42.75</div>
-              </div>
+            <div className="payment-summary-row">
+              <div>Items (3):</div>
+              <div className="payment-summary-money">$42.75</div>
+            </div>
 
-              <div className="payment-summary-row">
-                <div>Shipping &amp; handling:</div>
-                <div className="payment-summary-money">$4.99</div>
-              </div>
+            <div className="payment-summary-row">
+              <div>Shipping &amp; handling:</div>
+              <div className="payment-summary-money">$4.99</div>
+            </div>
 
-              <div className="payment-summary-row subtotal-row">
-                <div>Total before tax:</div>
-                <div className="payment-summary-money">$47.74</div>
-              </div>
+            <div className="payment-summary-row subtotal-row">
+              <div>Total before tax:</div>
+              <div className="payment-summary-money">$47.74</div>
+            </div>
 
-              <div className="payment-summary-row">
-                <div>Estimated tax (10%):</div>
-                <div className="payment-summary-money">$4.77</div>
-              </div>
+            <div className="payment-summary-row">
+              <div>Estimated tax (10%):</div>
+              <div className="payment-summary-money">$4.77</div>
+            </div>
 
-              <div className="payment-summary-row total-row">
-                <div>Order total:</div>
-                <div className="payment-summary-money">$52.51</div>
-              </div>
+            <div className="payment-summary-row total-row">
+              <div>Order total:</div>
+              <div className="payment-summary-money">$52.51</div>
+            </div>
 
-              <button className="place-order-button button-primary">
-                Place your order
-              </button>
+            <button className="place-order-button button-primary">
+              Place your order
+            </button>
           </div>
         </div>
       </div>
