e651364e3d371c91b467ac9e3889e7c0bbeb1322
NJ018 <nitinchillar015@gmail.com>
2026-09-19 16:44:25 +0530

    Add server config for /api and /images and also changed index.html

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,16 +4,16 @@ import './HomePage.css';
 import { Header } from '../components/Header';
 
 export function HomePage() {
-  const [products, setProducts]= useState();
-  const [cart, setCart]= useState();
+  const [products, setProducts]= useState([]);
+  const [cart, setCart]= useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/products')
+    axios.get('/api/products')
       .then((response) => {
         setProducts(response.data);
       });
 
-    axios.get('http://localhost:3000/api/cart-items')
+    axios.get('/api/cart-items')
       .then((response) => {
         setCart(response.data);
       })
