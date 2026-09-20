765cb68a5ff7d7dfe656a8332392690157900731
NJ018 <nitinchillar015@gmail.com>
2026-09-20 15:47:57 +0530

    navigating to orders page and making addtocart button interactie in orders page

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -5,17 +5,26 @@ import './OrdersPage.css';
 import { Header } from '../../components/Header';
 import { formatMoney } from '../../utils/money';
 
-
-export function OrdersPage({ cart }) {
+export function OrdersPage({ cart, loadCart }) {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    const fetchOrderData= async() => {
+    const fetchOrderData = async () => {
       const response = await axios.get('/api/orders?expand=products');
       setOrders(response.data);
-    }
+    };
+
     fetchOrderData();
-  }, [])
+  }, []);
+
+  const addToCart = async (productId, quantity) => {
+    await axios.post('/api/cart-items', {
+      productId,
+      quantity
+    });
+
+    await loadCart();
+  };
 
   return (
     <>
@@ -34,17 +43,31 @@ export function OrdersPage({ cart }) {
                 <div className="order-header">
                   <div className="order-header-left-section">
                     <div className="order-date">
-                      <div className="order-header-label">Order Placed:</div>
-                      <div>{dayjs(order.orderTimeMs).format('MMMM dddd D')}</div>
+                      <div className="order-header-label">
+                        Order Placed:
+                      </div>
+
+                      <div>
+                        {dayjs(order.orderTimeMs).format('MMMM dddd D')}
+                      </div>
                     </div>
+
                     <div className="order-total">
-                      <div className="order-header-label">Total:</div>
-                      <div>{formatMoney(order.totalCostCents)}</div>
+                      <div className="order-header-label">
+                        Total:
+                      </div>
+
+                      <div>
+                        {formatMoney(order.totalCostCents)}
+                      </div>
                     </div>
                   </div>
 
                   <div className="order-header-right-section">
-                    <div className="order-header-label">Order ID:</div>
+                    <div className="order-header-label">
+                      Order ID:
+                    </div>
+
                     <div>{order.id}</div>
                   </div>
                 </div>
@@ -53,24 +76,47 @@ export function OrdersPage({ cart }) {
                   {order.products.map((orderProduct) => {
                     return (
                       <Fragment key={orderProduct.product.id}>
+
                         <div className="product-image-container">
                           <img src={orderProduct.product.image} />
                         </div>
 
                         <div className="product-details">
+
                           <div className="product-name">
                             {orderProduct.product.name}
                           </div>
+
                           <div className="product-delivery-date">
-                            Arriving on: {dayjs(orderProduct.estimatedDeliveryTimeMs).format('MMMM D')}
+                            Arriving on:{' '}
+                            {dayjs(
+                              orderProduct.estimatedDeliveryTimeMs
+                            ).format('MMMM D')}
                           </div>
+
                           <div className="product-quantity">
                             Quantity: {orderProduct.quantity}
                           </div>
-                          <button className="buy-again-button button-primary">
-                            <img className="buy-again-icon" src="images/icons/buy-again.png" />
-                            <span className="buy-again-message">Add to Cart</span>
+
+                          <button
+                            className="buy-again-button button-primary"
+                            onClick={() => {
+                              addToCart(
+                                orderProduct.product.id,
+                                orderProduct.quantity
+                              );
+                            }}
+                          >
+                            <img
+                              className="buy-again-icon"
+                              src="images/icons/buy-again.png"
+                            />
+
+                            <span className="buy-again-message">
+                              Add to Cart
+                            </span>
                           </button>
+
                         </div>
 
                         <div className="product-actions">
@@ -80,11 +126,12 @@ export function OrdersPage({ cart }) {
                             </button>
                           </a>
                         </div>
+
                       </Fragment>
                     );
                   })}
-      
                 </div>
+
               </div>
             );
           })}
