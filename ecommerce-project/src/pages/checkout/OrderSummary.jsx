44c69bf0cc2cf12aa5dc4483291ea367cea66048
NJ018 <nitinchillar015@gmail.com>
2026-09-20 15:27:55 +0530

    make update button in cart interactive

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,9 +1,22 @@
 import dayjs from "dayjs";
 import axios from "axios";
+import { useState } from "react";
 import { formatMoney } from "../../utils/money";
 import { DeliveryOptions } from "./DeliveryOptions";
 
-export function OrderSummary({ cart, deliveryOptions, loadCart}) {
+export function OrderSummary({ cart, deliveryOptions, loadCart }) {
+  const [editingProductId, setEditingProductId] = useState(null);
+  const [quantity, setQuantity] = useState(1);
+
+  const updateQuantity = async (productId) => {
+    await axios.put(`/api/cart-items/${productId}`, {
+      quantity: Number(quantity)
+    });
+
+    await loadCart();
+    setEditingProductId(null);
+  };
+
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 && cart.map((cartItem) => {
@@ -12,7 +25,7 @@ export function OrderSummary({ cart, deliveryOptions, loadCart}) {
             return deliveryOption.id === cartItem.deliveryOptionId;
           });
 
-        const deleteCartItem= async() => {
+        const deleteCartItem = async () => {
           await axios.delete(`/api/cart-items/${cartItem.productId}`);
           await loadCart();
         };
@@ -36,18 +49,54 @@ export function OrderSummary({ cart, deliveryOptions, loadCart}) {
                   {formatMoney(cartItem.product.priceCents)}
                 </div>
                 <div className="product-quantity">
-                  <span>
-                    Quantity: <span className="quantity-label">
-                      {cartItem.quantity}
-                    </span>
-                  </span>
-                  <span className="update-quantity-link link-primary">
-                    Update
-                  </span>
-                  <span className="delete-quantity-link link-primary" 
-                    onClick={deleteCartItem}>
+
+                  {editingProductId === cartItem.productId ? (
+                    <>
+                      Quantity:
+                      <input
+                        type="number"
+                        className="quantity-input"
+                        min="1"
+                        max="10"
+                        value={quantity}
+                        onChange={(event) => {
+                          setQuantity(event.target.value);
+                        }}
+                      />
+
+                      <span
+                        className="update-quantity-link link-primary"
+                        onClick={() => updateQuantity(cartItem.productId)}
+                      >
+                        Save
+                      </span>
+                    </>
+                  ) : (
+                    <>
+                      Quantity:
+                      <span className="quantity-label">
+                        {cartItem.quantity}
+                      </span>
+
+                      <span
+                        className="update-quantity-link link-primary"
+                        onClick={() => {
+                          setEditingProductId(cartItem.productId);
+                          setQuantity(cartItem.quantity);
+                        }}
+                      >
+                        Update
+                      </span>
+                    </>
+                  )}
+
+                  <span
+                    className="delete-quantity-link link-primary"
+                    onClick={deleteCartItem}
+                  >
                     Delete
                   </span>
+
                 </div>
               </div>
 
